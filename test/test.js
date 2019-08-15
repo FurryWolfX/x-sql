65713126a51f7f4ded1dce896aa1f0714c8090fe
wolfx <digimonx@qq.com>
2019-08-15 22:46:46 +0800

    1.0.0

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const Builder = require("../lib/index");
+
+const builder = new Builder({
+  dir: path.resolve(__dirname, "./xml"),
+  debug: true,
+  debugCallback: log => {
+    console.log(log);
+  }
+});
+
+const result = builder.build("test", "getUser", {
+  flag: 1,
+  name: "wolfx",
+  idList: [
+    {
+      id: 3,
+    },
+    {
+      id: 5,
+    },
+  ],
+});
+console.log(result);
