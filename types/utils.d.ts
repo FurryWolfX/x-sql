d86132d0b12cf0b0ecec3c39b5110d1751fbb172
wolfx <digimonx@qq.com>
2019-08-16 23:28:15 +0800

    1.0.7

--- types/utils.d.ts
+++
@@ -1,7 +0,0 @@
-declare type FileItem = {
-    path?: string;
-    filename?: string;
-    url?: string;
-};
-export declare function readFileList(path: string, filesList?: any[]): FileItem[];
-export {};
